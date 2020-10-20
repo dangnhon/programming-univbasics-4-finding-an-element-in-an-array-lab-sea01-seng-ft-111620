43f749ceb7672feaea72b84ed20c9441effd8e0c
Nhon Dang <dang.nhon12345@yahoo.com>
2020-10-20 18:57:05 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
   count = 0
-  found_index = [] 
-  
-  while count < array.length do 
+  found_index = []
+
+  while count < array.length do
     found_index.push(array.index(value_to_find))
     count += 1
-  end 
+  end
   return found_index
-  
+
 end
